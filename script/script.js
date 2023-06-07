c231ef22510da2f5aa0d98d7615ed5d89a4ce919
Kladko Denys <kladko.denys@gmail.com>
2023-06-07 14:54:09 +0300

    add slider in our-team-block

--- script/script.js
+++ script/script.js
@@ -29,9 +29,56 @@ document.addEventListener('DOMContentLoaded', () => {
 
 	toggleMenuOpenClass('.modal-menu__title-close-trigger','.header');
 
+
 	//---------------------------
-	//--
+	//--Slider in Our-team
 	//---------------------------
+
+	
+	const slides = document.querySelectorAll('.team-card'),
+			previousSlideButton = document.querySelector('.our-team__team-card-block-slider-prew'),
+			nextSlideButton = document.querySelector('.our-team__team-card-block-slider-next'),
+			points = document.querySelectorAll('.comments__slider-points-gray-team');
+
+	let slideIndex = 0;
+
+	function hideSlides(){
+		slides.forEach((item) => item.style.display = 'none')
+	}
+
+	function showSlide (numberSlide) {
+		slides[numberSlide].style.display = 'block';
+	}
+
+	function addBluePoint (numberPoint) {
+		points.forEach((item) => item.classList.remove('comments__slider-points-blue'));
+		points[numberPoint].classList.add('comments__slider-points-blue');
+	}
+
+
+	previousSlideButton.addEventListener('click',function(){
+		slideIndex -= 1;
+
+		if ( slideIndex < 0){
+			slideIndex = slides.length - 1;
+		}
+
+		addBluePoint(slideIndex);
+		hideSlides();
+		showSlide(slideIndex);
+	})
+
+	nextSlideButton.addEventListener('click',function(){
+		slideIndex += 1;
+		
+		if ( slideIndex >= slides.length){
+			slideIndex = 0;
+		}
+
+		addBluePoint(slideIndex);
+		hideSlides();
+		showSlide(slideIndex);
+	})
 
 
 })
